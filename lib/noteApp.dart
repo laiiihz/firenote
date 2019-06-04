b438fa9686981551954ad0e13f6f49c07c5de6b6
laiiihz <laiiihz@outlook.com>
2019-06-04 15:04:49 +0800

    delete jpush support

--- lib/noteApp.dart
+++ lib/noteApp.dart
@@ -4,7 +4,6 @@ import 'package:scoped_model/scoped_model.dart';
 import 'model/appModel.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'Splash.dart';
-import 'package:jpush_flutter/jpush_flutter.dart';
 
 class NoteApp extends StatelessWidget {
   Future<bool> sharedDark() async {
@@ -13,29 +12,6 @@ class NoteApp extends StatelessWidget {
   }
   @override
   Widget build(BuildContext context) {
-    final JPush jPush=new JPush();
-    Future<void> initPush()async{
-      jPush.addEventHandler(
-        onReceiveNotification: (Map<String, dynamic> message) async {
-          print("flutter onReceiveNotification: $message");
-        },
-        // 点击通知回调方法。
-        onOpenNotification: (Map<String, dynamic> message) async {
-          print("flutter onOpenNotification: $message");
-        },
-        // 接收自定义消息回调方法。
-        onReceiveMessage: (Map<String, dynamic> message) async {
-          print("flutter onReceiveMessage: $message");
-        },
-      );
-      jPush.setup(
-        appKey: "6c3f37c8eb55fba38aa5ac72",
-        channel: "theChannel",
-        production: false,
-        debug: true,
-      );
-    }
-    initPush();
 
 
     return ScopedModel<AppModel>(
